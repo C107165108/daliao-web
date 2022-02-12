7a1de3cd223f8bb1ea567d91bb6521323e3cc969
C107165108 <C107165108@nkust.edu.tw>
2022-02-12 17:53:49 +0800

    整理 Newslist

--- src/JS/NewsPage/NewsList.js
+++ src/JS/NewsPage/NewsList.js
@@ -4,6 +4,7 @@ import  '../../Style/NewsPage/NewsList.css'
 export default function NewsList() {
     return (
         <div className="news-list-content">
+            <h3 className="news-list-title">最新消息</h3>
 
             <NewsItem />
             <NewsItem />
