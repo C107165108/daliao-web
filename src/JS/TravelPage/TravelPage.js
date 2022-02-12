e9369cf6cf95f48e0a5d175c82f7e0c629b86715
C107165108 <C107165108@nkust.edu.tw>
2022-02-12 18:07:27 +0800

    調整TravelPage

--- src/JS/TravelPage/TravelPage.js
+++ src/JS/TravelPage/TravelPage.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import TravelItem from './TravelItem';
+import TravelList from './TravelList';
+import '../../Style/TravelPage/TravelPage.css'
 
 
 export default class TravelPage extends React.Component {
     render() {
         return (
-            <div>
-                TravelPage
-                <TravelItem/>
+            <div className='travel-page-content'>
+                <h3 className="travel-page-title">小旅行</h3>
+                <div >
+                    <TravelList />
+                </div>
+
             </div>
         );
     }
---
+++ src/JS/TravelPage/Travellist.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import TravelItem from './TravelItem';
+import '../../Style/TravelPage/TravelList.css';
+
+export default class TravelList extends React.Component {
+    render() {
+        return (
+            <div className='travel-list-content'>
+
+                <TravelItem />
+                {/* <TravelItem />
+                <TravelItem />
+                <TravelItem />
+                <TravelItem />
+                <TravelItem /> */}
+            </div>
+        );
+    }
+
+}
